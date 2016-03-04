99c7095e3dba5ba5082019b6d2cda9dea44a59b1
Stefano Mandelli <linuxfree2@gmail.com>
2016-03-05 00:23:49 +0100

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   functions.R

--- functions.R
+++ functions.R
@@ -1,3 +1,4 @@
+#prova
 
 #This is a simple function for plotting on a file. Once you have the object "plot",
 #you pass it as an argument together with width, heigth and the name of the file where do you want to plot.
