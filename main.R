4e6d5d6f120389b2821c12bf8df74796a161bd41
Eugenio Thieme <eugen@thieme.it>
2016-03-05 10:14:59 +0100

    Changes to be committed: 	modified:   main.R 	modified:   scripts/convert_cvs.awk 	new file:   turbulence_class.R
    Ho aggiunto un file con la definizione in S4 di una possibile classe turbolenza,
    porta pazienza che anche io era la prima volta che la vedevo... ora sto definendo
    i vari metodi, ci sarà un sacco di roba da aggiustare ma almeno prima di uscire per Cunardo
    son riuscito a fare qualcosa.
    (Mi sono già segnato una lista di metodi da aggiungere, ora devo solo evitare di
    far casino con gli S4 e gli S3 a cui sono abituato)
    Grazie mille per lo script in awk ;)

--- main.R
+++ main.R
@@ -2,6 +2,14 @@ library(lattice)
 library(ggplot2)
 
 source('functions.R')
+source('turbulence_class.R')
+
+# Extracted data from csv using the script convert_cvs.awk. 
+data <- read.csv('./20160129.15r.dat')
+
+# Converted data (of class data.frame) into an object of class turbulence
+turb <- as.turbulence(data)
+
 
 
 # This is only an attempt to make a first main for our project, it should be changed 
