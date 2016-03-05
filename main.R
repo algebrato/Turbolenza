ff61fe4b0b0dcec0a59112452182fbe9ead54fc4
Eugenio Thieme <eugen@thieme.it>
2016-03-05 10:47:22 +0100

    Changes to be committed: 	modified:   main.R 	modified:   turbulence_class.R
    Ora la pianto veramente per oggi: la classe turbolenza è S4, ma il csv può venire
    importato come data.frame o matrix. Per questo sto scrivendo i vari metodi per
    castare nella classe turbolenza (poi i metodi di questa andranno scritti in S4
    come hai fatto nel modello di Ising, ma quello sarà un altro problema :P),
    non ho ancora finito ma data.frame e matrix ora vengono convertiti correttamente.
    Manca il metodo di default e magari uno per le liste, ma su quello ci penso un attimo.
    Buona giornata!

--- main.R
+++ main.R
@@ -10,7 +10,5 @@ data <- read.csv('./20160129.15r.dat')
 # Converted data (of class data.frame) into an object of class turbulence
 turb <- as.turbulence(data)
 
-
-
 # This is only an attempt to make a first main for our project, it should be changed 
 # during the work.
--- turbulence_class.R
+++ turbulence_class.R
@@ -4,13 +4,32 @@
 turbulence <- setClass('turbulence', slots=c(u='numeric', v='numeric', w='numeric', t='numeric'))
 
 
-# S3 method for casting a dataframe into an object of class turbulence
-# Now I must do the same for matrix, default and generic function
+# S3 method for casting an object into an object of class turbulence: generic function
+as.turbulence <- function(x, ...){
+  UseMethod('as.turbulence')
+}
 
-as.turbulence <- function(data){
+# S3 methods for casting a dataframe/matrix into an object of class turbulence
+# Now I must do the same for default and list
+as.turbulence.data.frame <- function(data){
   if(ncol(data)!=4) stop('Ncol is different from 4')
   if(ncol(data)==4){
     turb <- turbulence(u=data[,1], v=data[,2], w=data[,3], t=data[,4])
   }
   return(turb)
 }
+
+as.turbulence.matrix <- function(data){
+  if(ncol(data)!=4) stop('Ncol is different from 4')
+  if(ncol(data)==4){
+    turb <- turbulence(u=data[,1], v=data[,2], w=data[,3], t=data[,4])
+  }
+  return(turb)
+}
+
+# as.turbulence.default <- function(u, v, w, t){
+#   if(length(u)!=length(v) | length(u)!=length(w) | length(u)!=length(t)
+#       | length(v)!=length(w) | length(v)!=length(t) | length(w)!=length(t)){
+#         stop('Error, lengths differ')
+#       }
+# }
